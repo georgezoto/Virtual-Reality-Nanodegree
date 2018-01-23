342d9f3e363b5b18ba59b85d73e00119b0cfd54e
georgezoto <gezotos@gmail.com>
2018-01-23 00:28:48 -0500

    Unity 2017.2.0: Issue with creating many seagulls based on a reference using loops, in VR-Software-Development-Creating-Scripts-Course-Assets.

--- Term 1 - Foundations/VR Software Development/VR-Software-Development-Creating-Scripts-Course-Assets/Assets/ManyObjectMaker.cs
+++ Term 1 - Foundations/VR Software Development/VR-Software-Development-Creating-Scripts-Course-Assets/Assets/ManyObjectMaker.cs
@@ -13,10 +13,14 @@ public class ManyObjectMaker : MonoBehaviour {
 		for (int i = 0; i < 20; i++) {
 			// do something here
 			//Object.Instantiate(objectToCreate, new Vector3(i, 7 + Mathf.Sin(i), 3), Quaternion.identity);
+
 			Object.Instantiate(objectToCreate, new Vector3(5 + Mathf.Cos(i), 4.5f + Mathf.Sin(i), 3), Quaternion.identity);
 			Object.Instantiate(objectToCreate, new Vector3(11 + Mathf.Cos(i), 4.5f + Mathf.Sin(i), 3), Quaternion.identity);
 			Object.Instantiate(objectToCreate, new Vector3(i, 7 + Mathf.Sin(i), 3), Quaternion.identity);
 
+			//GameObject newSeagull = (GameObject)Object.Instantiate(objectToCreate, new Vector3(i, 0, 0), Quaternion.identity);
+			//Renderer objectRenderer = newSeagull.GetComponentInParent<Renderer> ();
+			//objectRenderer.material.color = Color.white * Random.value;
 
 		}
 	}
