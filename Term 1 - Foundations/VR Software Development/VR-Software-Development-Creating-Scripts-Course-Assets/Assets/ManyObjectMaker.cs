35b05a2e944e9177e21057834cdbcf3039cdaa41
georgezoto <gezotos@gmail.com>
2018-01-22 23:05:47 -0500

    Unity 2017.2.0: Create 20 objects based on a reference using loops, explore math in VR, have fun in VR-Software-Development-Creating-Scripts-Course-Assets.

---
+++ Term 1 - Foundations/VR Software Development/VR-Software-Development-Creating-Scripts-Course-Assets/Assets/ManyObjectMaker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManyObjectMaker : MonoBehaviour {
+
+	public GameObject objectToCreate;
+
+	// Use this for initialization
+	void Start () {
+		// make an object here
+		//Object.Instantiate(objectToCreate, new Vector3(2, 4, 6), Quaternion.identity);
+		for (int i = 0; i < 20; i++) {
+			// do something here
+			Object.Instantiate(objectToCreate, new Vector3(i, 7 + Mathf.Sin(i), 3), Quaternion.identity);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
