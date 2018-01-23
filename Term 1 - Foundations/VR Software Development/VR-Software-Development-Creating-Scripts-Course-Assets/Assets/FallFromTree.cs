f3a44c052ea0a064000f3e390859723d6b849fcb
georgezoto <gezotos@gmail.com>
2018-01-22 21:24:08 -0500

    Unity 2017.2.0: Use a script to make a coconut fall from a tree in the y axis in VR-Software-Development-Creating-Scripts-Course-Assets.

---
+++ Term 1 - Foundations/VR Software Development/VR-Software-Development-Creating-Scripts-Course-Assets/Assets/FallFromTree.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Move the coconut down every frame until it hits the ground, then stop.
+public class FallFromTree : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (0, -2.5f * Time.deltaTime, 0, Space.World);
+	}
+}
