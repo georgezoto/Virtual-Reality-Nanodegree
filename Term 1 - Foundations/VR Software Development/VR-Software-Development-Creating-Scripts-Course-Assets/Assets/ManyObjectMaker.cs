8f1a78f8ec3d75bd232bc91dec5a3db59ddf3281
georgezoto <gezotos@gmail.com>
2018-01-23 21:38:02 -0500

    Unity 2017.2.0: Issue with missing (Mono Script) in Lesson 2, Controlling Objects Using Code, in VR-Software-Development-Creating-Scripts-Course-Assets.

--- Term 1 - Foundations/VR Software Development/VR-Software-Development-Creating-Scripts-Course-Assets/Assets/ManyObjectMaker.cs
+++ Term 1 - Foundations/VR Software Development/VR-Software-Development-Creating-Scripts-Course-Assets/Assets/ManyObjectMaker.cs
@@ -12,11 +12,11 @@ public class ManyObjectMaker : MonoBehaviour {
 		//Object.Instantiate(objectToCreate, new Vector3(2, 4, 6), Quaternion.identity);
 		for (int i = 0; i < 20; i++) {
 			// do something here
-			//Object.Instantiate(objectToCreate, new Vector3(i, 7 + Mathf.Sin(i), 3), Quaternion.identity);
-
-			Object.Instantiate(objectToCreate, new Vector3(5 + Mathf.Cos(i), 4.5f + Mathf.Sin(i), 3), Quaternion.identity);
-			Object.Instantiate(objectToCreate, new Vector3(11 + Mathf.Cos(i), 4.5f + Mathf.Sin(i), 3), Quaternion.identity);
 			Object.Instantiate(objectToCreate, new Vector3(i, 7 + Mathf.Sin(i), 3), Quaternion.identity);
+
+			//Object.Instantiate(objectToCreate, new Vector3(5 + Mathf.Cos(i), 4.5f + Mathf.Sin(i), 3), Quaternion.identity);
+			//Object.Instantiate(objectToCreate, new Vector3(11 + Mathf.Cos(i), 4.5f + Mathf.Sin(i), 3), Quaternion.identity);
+			//Object.Instantiate(objectToCreate, new Vector3(i, 7 + Mathf.Sin(i), 3), Quaternion.identity);
 
 			//GameObject newSeagull = (GameObject)Object.Instantiate(objectToCreate, new Vector3(i, 0, 0), Quaternion.identity);
 			//Renderer objectRenderer = newSeagull.GetComponentInParent<Renderer> ();
