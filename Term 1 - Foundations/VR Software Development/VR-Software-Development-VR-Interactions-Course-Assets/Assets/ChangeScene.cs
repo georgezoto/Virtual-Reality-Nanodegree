c4729e1f362b16c0deb99c9272f612aa3925164b
georgezoto <gezotos@gmail.com>
2018-01-27 14:29:37 -0500

    Unity 2017.2.0: Update script ChangeScene.cs, add/drag the vacation box object as an input to the pointer click event trigger, in VR-Software-Development-VR-Interactions-Course-Assets.

--- Term 1 - Foundations/VR Software Development/VR-Software-Development-VR-Interactions-Course-Assets/Assets/ChangeScene.cs
+++ Term 1 - Foundations/VR Software Development/VR-Software-Development-VR-Interactions-Course-Assets/Assets/ChangeScene.cs
@@ -3,14 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ChangeScene : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	public void GoToScene(){
+		Debug.Log ("GoToScene() method called");
 	}
 }
