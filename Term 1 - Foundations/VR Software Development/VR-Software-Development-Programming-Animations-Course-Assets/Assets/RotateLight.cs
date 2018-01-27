a061aea38adaf3dff4feb005cde70ef74c35fd20
georgezoto <gezotos@gmail.com>
2018-01-27 18:22:08 -0500

    Unity 2017.2.0: The directional light, is rotated on its Y axis, by 180 degrees, using a script, directionalLight.transform.rotation  = Quaternion.Slerp (startRotation, endRotation, Time.time / 100f); in VR-Software-Development-Programming-Animations-Course-Assets.

--- Term 1 - Foundations/VR Software Development/VR-Software-Development-Programming-Animations-Course-Assets/Assets/RotateLight.cs
+++ Term 1 - Foundations/VR Software Development/VR-Software-Development-Programming-Animations-Course-Assets/Assets/RotateLight.cs
@@ -13,6 +13,8 @@ public class RotateLight : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		directionalLight.transform.Rotate (0f, Time.deltaTime, 0f);
+		Quaternion startRotation = Quaternion.Euler (50f, 30f, 0f);
+		Quaternion endRotation = startRotation * Quaternion.Euler (0f, 180f, 0f);
+		directionalLight.transform.rotation  = Quaternion.Slerp (startRotation, endRotation, Time.time / 100f);
 	}
 }
