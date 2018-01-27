fcfdac41c84e1698199fd44c8e9d90880953e304
georgezoto <gezotos@gmail.com>
2018-01-27 14:47:54 -0500

    Unity 2017.2.0: Update script ChangeScene.cs, add vacation box scene and falling cocnut scene in build settings, test scene change on click in VR-Software-Development-VR-Interactions-Course-Assets.

--- Term 1 - Foundations/VR Software Development/VR-Software-Development-VR-Interactions-Course-Assets/Assets/ChangeScene.cs
+++ Term 1 - Foundations/VR Software Development/VR-Software-Development-VR-Interactions-Course-Assets/Assets/ChangeScene.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour {
 	public void GoToScene(){
 		Debug.Log ("GoToScene() method called");
+		SceneManager.LoadScene ("00-FallingCoconut");
 	}
 }
