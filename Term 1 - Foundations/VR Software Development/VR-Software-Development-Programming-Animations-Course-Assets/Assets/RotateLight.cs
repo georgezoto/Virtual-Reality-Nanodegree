5a1c19a2e26b70c285984cff8150a4785c1b9ff6
georgezoto <gezotos@gmail.com>
2018-01-27 18:00:03 -0500

    Unity 2017.2.0: The directional light, is rotated on its Y axis using a script, directionalLight.transform.Rotate (0f, Time.deltaTime, 0f); in VR-Software-Development-Programming-Animations-Course-Assets.

---
+++ Term 1 - Foundations/VR Software Development/VR-Software-Development-Programming-Animations-Course-Assets/Assets/RotateLight.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateLight : MonoBehaviour {
+
+	public GameObject directionalLight;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		directionalLight.transform.Rotate (0f, Time.deltaTime, 0f);
+	}
+}
