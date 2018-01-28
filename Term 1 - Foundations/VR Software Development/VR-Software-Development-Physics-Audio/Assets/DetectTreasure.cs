15226b6b241191be51afadd0cb52771ab2584600
georgezoto <gezotos@gmail.com>
2018-01-28 01:08:52 -0500

    Unity 2017.2.0: Check that the treasure chest has a collider, update DetectTreasure.cs script, add script as a component to the MainCamera and let Unitys Ray casting detect the treasure for us in VR-Software-Development-Physics-Audio.

---
+++ Term 1 - Foundations/VR Software Development/VR-Software-Development-Physics-Audio/Assets/DetectTreasure.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectTreasure : MonoBehaviour {
+	
+	void FixedUpdate()
+	{
+		Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+		if (Physics.Raycast(transform.position, fwd, 10))
+			print("There is something in front of the object!");
+	}
+
+}
