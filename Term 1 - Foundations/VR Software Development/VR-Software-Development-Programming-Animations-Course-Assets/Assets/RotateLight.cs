4ada03801f38475e422b6c3a7cfb767c8d9fc426
georgezoto <gezotos@gmail.com>
2018-01-27 19:59:00 -0500

    Unity 2017.2.0: Modify RotateLight.cs script, add sun animator to the directional light, using SunAnimation that modifies the rotation and Light.intensity of the sun in VR-Software-Development-Programming-Animations-Course-Assets.

--- Term 1 - Foundations/VR Software Development/VR-Software-Development-Programming-Animations-Course-Assets/Assets/RotateLight.cs
+++ Term 1 - Foundations/VR Software Development/VR-Software-Development-Programming-Animations-Course-Assets/Assets/RotateLight.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class RotateLight : MonoBehaviour {
 
 	public GameObject directionalLight;
+	public Animator sunRotationAnimation;
+	bool isPressed = false; // Has the trigger button been pressed at least once?
+
 	float startTime = 0f;
 	int number = 42;
 	bool flag = true;
@@ -12,13 +15,25 @@ public class RotateLight : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		sunRotationAnimation.StartPlayback ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Quaternion startRotation = Quaternion.Euler (50f, 30f, 0f);
-		Quaternion endRotation = startRotation * Quaternion.Euler (0f, 180f, 0f);
-		directionalLight.transform.rotation  = Quaternion.Slerp (startRotation, endRotation, Time.time / 100f);
+		// // If the player pressed the cardboard button (or touched the screen) AND it has not already been pressed...
+		if (Input.GetMouseButtonDown (0) && !isPressed) {
+			// ...then update 'isPressed' to 'true'.
+			isPressed = true;
+			Debug.Log ("Update: 1");
+		}
+
+		// If the player pressed the cardboard button (or touched the screen) at least once...
+		if (isPressed) {
+			// ...then rotate.
+			sunRotationAnimation.StopPlayback ();
+			sunRotationAnimation.SetBool ("ChangeColor", true);
+			Debug.Log ("Update: 2");
+		}
 	}
+
 }
