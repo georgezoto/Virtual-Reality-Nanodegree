5e83187fb7e08ca0fb71a7ba5361a607effefadc
georgezoto <gezotos@gmail.com>
2018-01-28 23:37:40 -0500

    Unity 2017.2.0: Based on TriggerAnimation.cs from the apartment scene, update DetectTreasure.cs to set trigger after raycast has detected a collider to jump from Idle State to Chest_Upper_Part_Open and then to Close state in VR-Software-Development-Physics-Audio.

--- Term 1 - Foundations/VR Software Development/VR-Software-Development-Physics-Audio/Assets/DetectTreasure.cs
+++ Term 1 - Foundations/VR Software Development/VR-Software-Development-Physics-Audio/Assets/DetectTreasure.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DetectTreasure : MonoBehaviour {
-	
+
+	[Tooltip ("The Animator component on this gameobject")]
+	public Animator animator;
+	[Tooltip ("The name of the Animator trigger parameter")]
+	public string triggerName;
+
+
 	void FixedUpdate()
 	{
 		Vector3 fwd = transform.TransformDirection(Vector3.forward);
 
-		if (Physics.Raycast(transform.position, fwd, 10))
-			print("There is something in front of the object!");
+		if (Physics.Raycast (transform.position, fwd, 10)) {
+			print ("There is something in front of the object!");
+
+			// If the player pressed the cardboard button (or touched the screen), set the trigger parameter to active (until it has been used in a transition)
+			if (Input.GetMouseButtonDown (0)) {
+				animator.SetTrigger (triggerName);
+				print ("animator.SetTrigger (triggerName);");
+			}
+
+		}
 	}
 
 }
