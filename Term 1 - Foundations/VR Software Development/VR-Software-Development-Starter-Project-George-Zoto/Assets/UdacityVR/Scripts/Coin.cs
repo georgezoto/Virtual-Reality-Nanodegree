cc0511a1a53267a367149f48f74a7cdc9e8bda27
georgezoto <gezotos@gmail.com>
2018-01-31 22:29:22 -0500

    Unity 2017.2.0: Fix the position of a few Waypoints, Finally create a duplicate of the Poof prefab by dragging the original one from the Prefabs folder to the Hierarchy, rename it and then drag it back to Prefabs folder Attach the CoinPoof prefab to the Coin prefab plus the coin audio file, update Coin.cs script with Object.Instantiate(CoinPoof, transform.position, Quaternion.Euler(-90, 0, 0)); Guidelines followed from discussion at https://discussions.udacity.com/t/coins-and-poofs/204917/22 in VR-Software-Development-Starter-Project-George-Zoto.

--- Term 1 - Foundations/VR Software Development/VR-Software-Development-Starter-Project-George-Zoto/Assets/UdacityVR/Scripts/Coin.cs
+++ Term 1 - Foundations/VR Software Development/VR-Software-Development-Starter-Project-George-Zoto/Assets/UdacityVR/Scripts/Coin.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class Coin : MonoBehaviour 
 {
     //Create a reference to the CoinPoofPrefab
+	public GameObject CoinPoof;
 
     public void OnCoinClicked() {
         // Instantiate the CoinPoof Prefab where this coin is located
         // Make sure the poof animates vertically
         // Destroy this coin. Check the Unity documentation on how to use Destroy
+		print("Coin.cs: OnCoinClicked()");
+		Object.Instantiate(CoinPoof, transform.position, Quaternion.Euler(-90, 0, 0));
+		Destroy (gameObject);
     }
 
 }
