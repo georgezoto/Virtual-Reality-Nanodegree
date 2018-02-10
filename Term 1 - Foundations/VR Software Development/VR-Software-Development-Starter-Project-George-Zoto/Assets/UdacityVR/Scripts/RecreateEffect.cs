49b3f21ad4a3a20993815928d387496ae65c532b
georgezoto <gezotos@gmail.com>
2018-02-10 13:06:26 -0500

    Unity 2017.2.0: Fixed Issue with visual effect (sound is following parent object) not following parent object, spaceship in this case, during an animation or during game play in VR-Software-Development-Starter-Project-George-Zoto.
    Make a particle system rotate with it's parent?
    https://forum.unity.com/threads/make-a-particle-system-rotate-with-its-parent.445052/#post-3387167
    
    How to make a single particle apear with no delay
    https://answers.unity.com/questions/356412/how-to-make-a-single-particle-apear-with-no-delay.html

---
+++ Term 1 - Foundations/VR Software Development/VR-Software-Development-Starter-Project-George-Zoto/Assets/UdacityVR/Scripts/RecreateEffect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecreateEffect : MonoBehaviour {
+
+	public GameObject Effect;
+
+	// Use this for initialization
+	void Start () {
+		//Object.Instantiate(Effect, transform.position, Quaternion.identity);
+		//Destroy (Effect);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print ("Update!");
+		//transform.Translate (0f, Time.deltaTime*10, 0f, Space.World);
+	}
+}
